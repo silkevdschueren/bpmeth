e9ff360d874db6a9390713e362c6afbf04b7cf2b
Silke <silkevdschueren@gmail.com>
2026-06-03 18:16:06 +0200

    Adding test because there is bug in bs with h

--- tests/test_fast_hamilton_rk4.py
+++ tests/test_fast_hamilton_rk4.py
@@ -125,8 +125,32 @@ def test_combined_function_track():
     assert np.isclose(p1.py, p2.py)
     assert np.isclose(p1.delta, p2.delta)
     assert np.isclose(p1.zeta, p2.zeta)
-    
 
+def test_solenoid_track():
+    comp = np.zeros((9, 4), dtype=np.float64)
+    comp[0, 0] = 0.1
+    bs, b, a = comp_to_sympy(comp)
+    h = 0.2
+    length = 0.1
+    
+    PolyMagnet = bpmeth.PolySegment(length=length, h=h, comp=comp)
+    A_magnet = bpmeth.GeneralVectorPotential(h=h, bs=bs)
+    H_magnet = bpmeth.Hamiltonian(length=length, h=h, vectp=A_magnet)
+    
+    p0 = xt.Particles(x=0.1, px=0.01, y=0.2, py=0.03, delta=0.1, zeta=0.01)
+    p1 = p0.copy()
+    p2 = p0.copy()
+    
+    PolyMagnet.track(p1)
+    H_magnet.track(p2)
+    
+    assert np.isclose(p1.x, p2.x)
+    assert np.isclose(p1.px, p2.px)
+    assert np.isclose(p1.y, p2.y)
+    assert np.isclose(p1.py, p2.py)
+    assert np.isclose(p1.delta, p2.delta)
+    assert np.isclose(p1.zeta, p2.zeta)
+    
 def test_sdep_track():
     comp = np.zeros((9, 4), dtype=np.float64)
     comp[1, 0] = 0.1
