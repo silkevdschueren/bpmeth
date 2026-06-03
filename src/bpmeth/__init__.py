b734b89020aa1174428ec3025b4a868db62f9399
Silke <silkevdschueren@gmail.com>
2026-06-03 18:15:26 +0200

    Fixing bug in initialize if polyhamiltonian source code does not exist

--- src/bpmeth/__init__.py
+++ src/bpmeth/__init__.py
@@ -5,17 +5,18 @@ from .numerical_solver import *
 from .track4d import *
 from .fringefieldmaps import *
 from .fieldmaps import *
-from .magnet import *
-from . import poly_fit
-
 from pathlib import Path
-ab_order, sorder = 4, 3
+
+ab_order, sorder = 3, 3
 package_dir = Path(__file__).resolve().parent
 filename = f"polyhamiltonian_{ab_order}_{sorder}_h.py"
 file_path = package_dir / filename
+print(file_path)
 if not file_path.exists():
     print("Building tracking code...")
     from .fast_hamilton_solver_create_sourcecode import mk_field
     mk_field(ab_order=ab_order, sorder=sorder, h=True, nphi=3, out=str(file_path))
 
+from .magnet import *
+from . import poly_fit
 from .fast_hamilton_solver import *
