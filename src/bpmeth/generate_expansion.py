052e11388a1c938b758eda2c8eba6a6517f3fe18
Silke <silkevdschueren@gmail.com>
2026-08-27 16:36:17 +0200

    Fix sign mistake for solenoid!

--- src/bpmeth/generate_expansion.py
+++ src/bpmeth/generate_expansion.py
@@ -73,7 +73,7 @@ class FieldExpansion:
         h = self.h
         nphi = self.nphi
         
-        phi0 = -sum((an * x ** (n + 1) / sp.factorial(n + 1) for n, an in enumerate(a))) + sp.integrate(bs, s)
+        phi0 = -sum((an * x ** (n + 1) / sp.factorial(n + 1) for n, an in enumerate(a))) - sp.integrate(bs, s)
         phi1 = -sum((bn * x**n / sp.factorial(n) for n, bn in enumerate(b)))        
         phiv = [phi0, phi1]
         for i in range(nphi-2):
